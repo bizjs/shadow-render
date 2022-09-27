a242c10f065e5c86a641ff66570c27e59892e6bc
HstarStudio <hm910705@163.com>
2022-09-27 22:43:59 +0800

    feat: update demo code

--- examples/src/pages/index.tsx
+++ examples/src/pages/index.tsx
@@ -1,9 +1,31 @@
 import { ShadowRender } from '@bizjs/shadow-render-react';
+import { useState } from 'react';
+
+const HTML_CONTENT = '<h1>我是 h1 内容</h1>';
 
 export default function HomePage() {
+  const [htmlContent, setHtmlContent] = useState(HTML_CONTENT);
   return (
     <div>
-      <ShadowRender htmlContent="<h1>H1</h1>" />
+      <div>
+        <button
+          onClick={() => {
+            setHtmlContent(HTML_CONTENT);
+          }}
+        >
+          渲染 HTML Content
+        </button>
+        <button
+          onClick={() => {
+            setHtmlContent(undefined);
+          }}
+        >
+          渲染 Children
+        </button>
+      </div>
+      <ShadowRender htmlContent={htmlContent}>
+        <h2 style={{ color: 'red' }}>我是 h2 内容 - Children</h2>
+      </ShadowRender>
     </div>
   );
 }
